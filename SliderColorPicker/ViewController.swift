b20ba9e4ecdbb8702ef20d96d3b49947f11399d6
Rohit Gurnani <rkgi10@gmail.com>
2015-05-26 18:36:11 +0530

    First commit . All features have been added. Just improvements to UI using class sizes left.

--- SliderColorPicker/ViewController.swift
+++ SliderColorPicker/ViewController.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var bgcolor: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    
+    
+    @IBOutlet var backgroundcolorlabel: UILabel!
+    
+    @IBOutlet var hexcode: UILabel!
+    @IBOutlet var hexcodelabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        redSlider.continuous = true
+        blueSlider.continuous = true
+        greenSlider.continuous = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func changeBackground(sender: AnyObject) {
+        var red = redSlider.value
+        var blue = blueSlider.value
+        var green = greenSlider.value
+        
+        hexcode.text = String(format: "%02X", Int(red))+String(format: "%02X", Int(green))+String(format: "%02X", Int(blue))
+        println(hexcode.text)
+        
+        
+        
+        bgcolor.text = "(\(Int(red)), \(Int(green)), \(Int(blue)))"
+                var r : CGFloat = CGFloat(red/255)
+        var b = CGFloat(blue/255)
+        var g = CGFloat(green/255)
+        self.view.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
+        
+        //getting inverse UIColor for background color value
+        let inversecolor = getInverseColor(r, g: g, b: b)
+        bgcolor.textColor = UIColor(red: inversecolor.rinverse, green: inversecolor.ginverse, blue: inversecolor.binverse, alpha: 1.0)
+        backgroundcolorlabel.textColor = UIColor(red: inversecolor.rinverse, green: inversecolor.ginverse, blue: inversecolor.binverse, alpha: 1.0)
+        hexcodelabel.textColor = UIColor(red: inversecolor.rinverse, green: inversecolor.ginverse, blue: inversecolor.binverse, alpha: 1.0)
+        hexcode.textColor = UIColor(red: inversecolor.rinverse, green: inversecolor.ginverse, blue: inversecolor.binverse, alpha: 1.0)
+
+        
+        
+    }
+    
+    func getInverseColor(r : CGFloat,g : CGFloat,b : CGFloat) -> (rinverse : CGFloat , ginverse :CGFloat, binverse : CGFloat ){
+        //
+        var rinverse = 1.0 - r
+        var binverse = 1.0 - b
+        var ginverse = 1.0 - g
+        return (rinverse, ginverse , binverse)
     }
 
 
